fc87d6c6d16a4e9f383afd80ff758de78f5ed2eb
lxhroot <lxh94088@petalmail.com>
2022-11-30 20:50:50 +0800

    学V0.8.4

--- js/index.js
+++ js/index.js
@@ -2,7 +2,7 @@
  * @Author: error: git config user.name && git config user.email & please set dead value or install git
  * @Date: 2022-10-22 23:53:53
  * @LastEditors: error: git config user.name && git config user.email & please set dead value or install git
- * @LastEditTime: 2022-11-23 16:47:45
+ * @LastEditTime: 2022-11-30 14:41:44
  * @FilePath: \Semester projects\js\index.js
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
@@ -185,4 +185,9 @@ $(function() {
     })
 
    })
+
+   $('.b1').click(function(){
+    window.location.href = "ShopCar.html";
+   })
+   
 })
